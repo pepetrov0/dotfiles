c874f6892c34c3cd1e8b07c8b812a5bc7037b290
Petar Petrov <ppetrov@noreply.pbyte.xyz>
2024-01-04 00:27:53 +0200

    Update .chezmoiscripts/run_once_before_0025-set-noto-fonts-as-system-defaults.sh

--- .chezmoiscripts/run_once_before_0025-set-noto-fonts-as-system-defaults.sh
+++ .chezmoiscripts/run_once_before_0025-set-noto-fonts-as-system-defaults.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gsettings set org.gnome.desktop.interface font-name "NotoSans Nerd Font Condensed 11"
-gsettings set org.gnome.desktop.interface document-font-name "NotoSans Nerd Font Condensed 11"
-gsettings set org.gnome.desktop.wm.preferences titlebar-font "NotoSans Nerd Font Semi-Bold Condensed 11"
+gsettings set org.gnome.desktop.interface font-name "NotoSans Nerd Font Regular 11"
+gsettings set org.gnome.desktop.interface document-font-name "NotoSans Nerd Font Regular 11"
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "NotoSans Nerd Font Semi-Bold 11"
